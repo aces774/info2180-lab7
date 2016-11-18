280d65cc0c1813727a05a167e2ae24aa39ac7289
aces774 <krystan09@yahoo.com>
2016-11-18 00:12:41 -0500

    Update world.php

--- world.php
+++ world.php
@@ -8,15 +8,6 @@ $username = getenv('C9_USER');
 $password = '';
 $dbname = 'world';
 
-//$conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-//$stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
-//$results = $stmt->fetchALL(PDO::FETCH_ASSOC);
-//echo '<ul>';
-//foreach ($results as $row) {
-//  echo '<li>' . $row['name'] . ' is ruled by ' . $row['head_of_state'] . '</li>';
-//}
-//echo '</ul>';
-
 if ($all == true || empty($country)){
     $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
     $stmt = $conn->query("SELECT * FROM countries");
